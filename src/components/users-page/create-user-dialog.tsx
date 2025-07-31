3c710ac137bcef53e1d338dbeac18905d1b31962
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:52:34 -0300

    Add create-user-dialog.tsx

---
+++ src/components/users-page/create-user-dialog.tsx
@@ -0,0 +1,129 @@
+"use client"
+
+import { FC, ReactNode } from "react"
+
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { useCreateUserForm } from "@/hooks/forms/use-create-user-form"
+import { CreateUserDto } from "@/dto/users/create-user"
+import { UserRole } from "../../enums/users/role"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+type Props = {
+    children: ReactNode
+}
+
+const CreateUserDialog: FC<Props> = ({ children }) => {
+    const form = useCreateUserForm()
+
+    const onSubmit = (data: CreateUserDto) => {}
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent className="space-y-6">
+                <DialogHeader>
+                    <DialogTitle>Criar usuário</DialogTitle>
+                    <DialogDescription>Insira os dados do novo usuário para criá-lo.</DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-x-2 gap-y-6">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nome *</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Marcuth" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="role"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Cargo *</FormLabel>
+                                    <FormControl>
+                                        <Select value={field.value} onValueChange={field.onChange}>
+                                            <SelectTrigger className="w-full">
+                                                <SelectValue placeholder="Selecione um cargo" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                    <SelectLabel>Cargos de usuário</SelectLabel>
+                                                    {Object.values(UserRole).map((role) => (
+                                                        <SelectItem value={role} key={role}>
+                                                            {role}
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username *</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="marcuth" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha *</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="••••••••" type="password" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="outline">Cancelar</Button>
+                    </DialogClose>
+                    <Button type="submit">Criar usuário</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default CreateUserDialog
