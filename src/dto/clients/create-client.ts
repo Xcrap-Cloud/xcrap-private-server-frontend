0078a2a0e3f2614ce3b9809be9f2eda11d0ddb52
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:20 -0300

    Update create-client.ts

--- src/dto/clients/create-client.ts
+++ src/dto/clients/create-client.ts
@@ -1,13 +1,19 @@
-import { IsEnum, IsString } from "class-validator"
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator"
 import "reflect-metadata"
+
+import { Transform } from "class-transformer"
 
 import { ClientType } from "@/enums/clients/client-type"
 
 export class CreateClientDto {
     @IsString()
+    @IsNotEmpty()
     name: string
 
+    @IsOptional()
     @IsString()
+    @IsNotEmpty()
+    @Transform(({ value }) => (value === "" ? undefined : value))
     description: string
 
     @IsEnum(ClientType)
