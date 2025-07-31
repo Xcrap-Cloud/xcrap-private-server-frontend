0702135b17e9aaa4da722aa5234daabcdbe18308
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:19 -0300

    Update create-client-form.tsx

--- src/components/layout/create-client-page/create-client-form.tsx
+++ src/components/layout/create-client-page/create-client-form.tsx
@@ -1,27 +1,45 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { LuPlus } from "react-icons/lu"
-import { FC } from "react"
+import { FC, useState } from "react"
+import { toast } from "sonner"
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import useCreateClientForm from "@/hooks/forms/use-create-client-form"
 import { CreateClientDto } from "@/dto/clients/create-client"
 import { renderClientType } from "@/utils/render-client-type"
+import { createClient } from "../../../services/api/clients"
 import { ClientType } from "@/enums/clients/client-type"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import Spinner from "../../ui/spinner"
 
 type Props = {
     accessToken: string
 }
 
 const CreateClientForm: FC<Props> = ({ accessToken }) => {
+    const router = useRouter()
     const form = useCreateClientForm()
+    const [isFetching, setIsFething] = useState(false)
 
-    const onSubmit = (data: CreateClientDto) => {
-        console.log(data)
+    const onSubmit = async (data: CreateClientDto) => {
+        setIsFething(true)
+
+        try {
+            toast.info(`Criando cliente "${data.name}"...`)
+            const createdClient = await createClient(data, accessToken)
+            toast.success("Cliente criado com sucesso!")
+            router.push(`/clients/${createdClient.id}`)
+        } catch (error) {
+            console.error("Error when trying create client:", error)
+            toast.error("Ocorreu um erro ao tentar criar o cliente!")
+        } finally {
+            setIsFething(false)
+        }
     }
 
     return (
@@ -78,8 +96,21 @@ const CreateClientForm: FC<Props> = ({ accessToken }) => {
                     )}
                 />
                 <div className="col-span-2">
-                    <Button>
-                        <LuPlus /> Criar Cliente
+                    <Button
+                        type="submit"
+                        className={`${isFetching ? "animate-pulse" : ""} flex gap-1 items-center`}
+                        disabled={isFetching}
+                    >
+                        {isFetching ? (
+                            <>
+                                <Spinner />
+                                Criando Cliente...
+                            </>
+                        ) : (
+                            <>
+                                <LuPlus /> Criar Cliente
+                            </>
+                        )}
                     </Button>
                 </div>
             </form>
