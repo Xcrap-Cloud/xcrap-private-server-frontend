ea58dbd8236c8797dd193a899ee5185fcfab6b9c
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:20 -0300

    Update top-section.tsx

--- src/components/layout/scrapers-page/top-section.tsx
+++ src/components/layout/scrapers-page/top-section.tsx
@@ -1,13 +1,22 @@
+import { LuCirclePlus } from "react-icons/lu"
+import NextLink from "next/link"
 import { FC } from "react"
 
 import { Typography } from "@/components/ui/typography"
+import { Button } from "@/components/ui/button"
 
-type Props = {}
-
-const ScrapersPageTopSection: FC<Props> = ({}) => {
+const ScrapersPageTopSection: FC = () => {
     return (
         <section className="pb-4">
-            <Typography.H1>Scrapers</Typography.H1>
+            <div className="flex items-center justify-between">
+                <Typography.H1>Scrapers</Typography.H1>
+                <Button asChild size="sm">
+                    <NextLink href="/scrapers/create">
+                        <LuCirclePlus />
+                        Criar scraper
+                    </NextLink>
+                </Button>
+            </div>
             <Typography.Muted className="mt-4 text-sm">
                 Esta é a listagem dos Scrapers criados, você também pode navegar para a página de cada Scraper ou criar
                 um Scraper novo.
