1bca8cf1e4f650cab32cab6c6bd63d5c00727b60
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:52:33 -0300

    Update page.tsx

--- src/app/(dashboard)/admin/users/page.tsx
+++ src/app/(dashboard)/admin/users/page.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react"
 
-import { getAuthenticatedSession } from "../../../../utils/get-access-token"
-import { findManyUsers } from "../../../../services/api/users"
+import UsersPageTopSection from "@/components/users-page/top-section"
+import { getAuthenticatedSession } from "@/utils/get-access-token"
+import { findManyUsers } from "@/services/api/users"
 
 const UsersPage: FC = async () => {
     const { accessToken } = await getAuthenticatedSession()
 
     const initialData = await findManyUsers(undefined, accessToken)
 
-    return <div className="max-w-7xl mx-auto py-6"></div>
+    return (
+        <div className="max-w-7xl mx-auto py-6">
+            <UsersPageTopSection />
+        </div>
+    )
 }
 
 export default UsersPage
