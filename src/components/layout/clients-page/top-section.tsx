43071b2d7fde500294695d66f39a29f00c77b90a
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:19 -0300

    Update top-section.tsx

--- src/components/layout/clients-page/top-section.tsx
+++ src/components/layout/clients-page/top-section.tsx
@@ -1,11 +1,23 @@
+import NextLink from "next/link"
 import { FC } from "react"
 
+import { LuCirclePlus } from "react-icons/lu"
+
 import { Typography } from "@/components/ui/typography"
+import { Button } from "@/components/ui/button"
 
 const ClientsPageTopSection: FC = () => {
     return (
         <section className="pb-4">
-            <Typography.H1>Clientes</Typography.H1>
+            <div className="flex items-center justify-between">
+                <Typography.H1>Clientes</Typography.H1>
+                <Button asChild size="sm">
+                    <NextLink href="/clients/create">
+                        <LuCirclePlus />
+                        Criar cliente
+                    </NextLink>
+                </Button>
+            </div>
             <Typography.Muted className="mt-4 text-sm">
                 Esta é a listagem dos Clientes criados, você também pode navegar para a página de cada Cliente ou criar
                 um Cliente novo.
