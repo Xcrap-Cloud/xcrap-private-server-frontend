1f1165dad0183b419805d67046e8de788bbd1098
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:21 -0300

    Update client-type.ts

--- src/enums/clients/client-type.ts
+++ src/enums/clients/client-type.ts
@@ -1,5 +1,9 @@
 export enum ClientType {
-    got_scraping = "got_scraping",
     axios = "axios",
     http_core = "http_core",
+    got_scraping = "got_scraping",
+    impit = "impit",
+    // puppeteer = "puppeteer",
+    // puppeteer_extra = "puppeteer_extra",
+    // puppeteer_real_browser = "puppeteer_real_browser",
 }
