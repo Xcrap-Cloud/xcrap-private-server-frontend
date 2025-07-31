0c360c70a88f6fc0e21840d3072add507654b91d
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:20:19 -0300

    Update client-section.tsx

--- src/components/layout/client-page/client-section.tsx
+++ src/components/layout/client-page/client-section.tsx
@@ -74,9 +74,7 @@ const ClientPageSection: FC<Props> = ({ data, accessToken }) => {
                         <Avatar>
                             <AvatarFallback>{data.owner.name[0].toUpperCase()}</AvatarFallback>
                         </Avatar>
-                        <Typography.Muted>
-                            <Link href={`/users/${data.owner.id}`}>{data.owner.name}</Link>
-                        </Typography.Muted>
+                        <Typography.Muted>{data.owner.name}</Typography.Muted>
                     </div>
                 </CardFooter>
             </Card>
