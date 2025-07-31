4b4a09929fe737f1756c8f81ef00cf6a3a3c8e6d
Marcuth <thiago.playgames15@gmail.com>
2025-07-31 03:52:34 -0300

    Update config.ts

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -4,6 +4,14 @@ const configHelper = {
         defaultId: "3a58a5e2-9b08-4860-b8bd-501898d57317",
         perPageInSelector: 4,
     },
+    users: {
+        minNameLength: 3,
+        maxNameLength: 100,
+        minUsernameLength: 4,
+        maxUsernameLength: 16,
+        maxEmailLength: 150,
+        generatedUsernameLength: 8,
+    },
 }
 
 export default configHelper
